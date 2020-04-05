bafb80c7deb72ded2c13d0f85ec43f0298b902b8
wanghefei <306623777@qq.com>
2020-04-05 22:35:15 +0800

    11111

---
+++ libs/testDates.js
@@ -0,0 +1,173 @@
+export const applyList = [
+  {
+    name:'飘柔洗发水',
+    category:'清洁用品',
+    price:'99',
+    state:1,
+    applyTime:'2020-03-03'
+  },
+  {
+    name: '中华牙膏',
+    category: '清洁用品',
+    price: '199',
+    state: 2,
+    applyTime: '2020-04-03'
+  },
+  {
+    name: '飘柔洗发水',
+    category: '清洁用品',
+    price: '99',
+    state: 3,
+    applyTime: '2020-03-03'
+  },
+  {
+    name: '中华牙膏',
+    category: '清洁用品',
+    price: '199',
+    state:4,
+    applyTime: '2020-04-03'
+  },
+  {
+    name: '中华牙膏',
+    category: '清洁用品',
+    price: '199',
+    state: 1,
+    applyTime: '2020-04-03'
+  },
+  {
+    name: '飘柔洗发水',
+    category: '清洁用品',
+    price: '99',
+    state: 2,
+    applyTime: '2020-03-03'
+  },
+  {
+    name: '中华牙膏',
+    category: '清洁用品',
+    price: '199',
+    state: 3,
+    applyTime: '2020-04-03'
+  }
+]
+export const ysList = [
+  {
+    state:'1',
+    name:'漂流洗发水',
+    ysPrice:'2000',
+    ssPrice:'3000',
+    startTime:'2020-03-03'
+  },
+  {
+    state: '2',
+    name: '海飞丝',
+    ysPrice: '1000',
+    ssPrice: '4000',
+    startTime: '2020-03-03'
+  },
+  {
+    state: '3',
+    name: '海飞丝',
+    ysPrice: '1000',
+    ssPrice: '4000',
+    startTime: '2020-03-03'
+  },
+  {
+    state: '4',
+    name: '海飞丝',
+    ysPrice: '1000',
+    ssPrice: '4000',
+    startTime: '2020-03-03'
+  },
+  {
+    state: '1',
+    name: '海飞丝',
+    ysPrice: '1000',
+    ssPrice: '4000',
+    startTime: '2020-03-03'
+  },
+  {
+    state: '2',
+    name: '海飞丝',
+    ysPrice: '1000',
+    ssPrice: '4000',
+    startTime: '2020-03-03'
+  },
+  {
+    state: '3',
+    name: '海飞丝',
+    ysPrice: '1000',
+    ssPrice: '4000',
+    startTime: '2020-03-03'
+  },
+  {
+    name: '海飞丝',
+    ysPrice: '1000',
+    ssPrice: '4000',
+    startTime: '2020-03-03'
+  },
+  {
+    name: '海飞丝',
+    ysPrice: '1000',
+    ssPrice: '4000',
+    startTime: '2020-03-03'
+  }
+]
+export const monthList = ['一月', '二月', '三月', '四月', '五月', '，六月', '七月', '八月', 
+'九月', '，十月', '十一月', '十二月']
+
+export const categoryList=[
+  {
+    value:1,
+    label:'清洁用品'
+  },
+  {
+    value: 2,
+    label: '床上用品'
+  },
+  {
+    value: 3,
+    label: '化妆品'
+  }
+]
+export const payList = [
+  {
+    name: '飘柔洗发水',
+    category: '清洁用品',
+    price: '99',
+    state: 1,
+    applyTime: '2020-03-03'
+  },
+  {
+    name: '中华牙膏',
+    category: '清洁用品',
+    price: '199',
+    state: 2,
+    applyTime: '2020-04-03'
+  },
+]
+export const skList = [
+  {
+    name:'收账2000',
+    ysPrice:'3000',
+    ssPrice:'2000',
+    sTime:'2020-03-03'
+  },
+  {
+    name: '收账3000',
+    ysPrice: '3000',
+    ssPrice: '2000',
+    sTime: '2020-03-03'
+  },
+  {
+    name: '收账4000',
+    ysPrice: '3000',
+    ssPrice: '2000',
+    sTime: '2020-03-03'
+  },
+  {
+    name: '收账5000',
+    ysPrice: '3000',
+    ssPrice: '2000',
+    sTime: '2020-03-03'
+  }
+]

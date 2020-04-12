5de4fa3f4789ea165381c5474d7e775e4192a163
cxs <2462523702@qq.com>
2020-04-12 14:16:21 +0800

    月份选择器

---
+++ components/monthPicker/monthPicker.vue
@@ -0,0 +1,113 @@
+<template>
+	<view v-if="show" class="v-time-picker">
+		<view @tap.stop="onCancel" style="height: 100%;width: 100%;"></view>
+		<!-- 时间选择器 -->
+		<view class="v-title">
+			<text class="sel-btn" @tap.stop="onCancel">取消</text>
+			<text class="time-picker-title">选择时间</text>
+			<text class="sel-btn" @tap="onConfirm">确定</text>
+		</view>
+		<picker-view class="mpvue-picker-view" :indicator-style="indicatorStyle" :value="value" @change="bindChange">
+			<picker-view-column>
+				<view class="item" v-for="(item,index) in months" :key="index">{{item}}月</view>
+			</picker-view-column>
+		</picker-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			requestCode: Number, // 选择时间的请求code
+			show: Boolean,
+			month: Number 
+		},
+		data() {
+			const months = []
+			for (let i = 1; i <= 12; i++) {
+				months.push(i)
+			}
+			return {
+				months,
+				value: [0],
+				visible: true,
+				indicatorStyle: `height: ${Math.round(uni.getSystemInfoSync().screenWidth/(750/100))}px;`
+			}
+		},
+		methods: {
+			bindChange(e) {
+				const val = e.detail.value;
+				this.value = val;
+			},
+			onConfirm() {
+				this.$emit("onConfirm", {
+					code: this.requestCode,
+					month: this.value[0]
+				})
+			},
+			onCancel() {
+				this.$emit("onCancel")
+			}
+		},
+		watch: {
+			month: {
+				handler(newVal, oldVal) {
+					this.value[0] = newVal;
+				},
+				immediate: true,
+				deep: true
+			}
+		}
+	}
+</script>
+
+<style>
+	.v-time-picker {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		width: 100%;
+		height: 100%;
+		position: fixed;
+		top: 0;
+		left: 0;
+		background-color: rgba(0, 0, 0, 0.7);
+
+		.v-title {
+			display: flex;
+			width: 100%;
+			height: 100rpx;
+			background-color: #FFFFFF;
+			border-bottom: solid #ededed 1rpx;
+
+			.sel-btn {
+				width: 15%;
+				font-size: 30rpx;
+				color: #1a1a1a;
+				text-align: center;
+				line-height: 100rpx;
+			}
+
+			.time-picker-title {
+				flex: 1;
+				font-size: 40rpx;
+				text-align: center;
+				line-height: 100rpx;
+			}
+		}
+
+		.mpvue-picker-view {
+			width: 750rpx;
+			height: 470rpx;
+			background-color: rgba(255, 255, 255, 1);
+
+			.item {
+				text-align: center;
+				text-overflow: ellipsis;
+				line-height: 90rpx;
+				font-size: 36rpx;
+				color: #1a1a1a;
+			}
+		}
+	}
+</style>
--- pages/reportScope/reportScope.vue
+++ pages/reportScope/reportScope.vue
@@ -4,7 +4,7 @@
 		<view class="v-time-range">
 			<text class="time-range" @tap="onStartTimeTap(0,startTime)">
 				<text v-if="startTime==''" style="font-size: 30rpx;color: #cccccc;">选择开始时间</text>
-				{{startTime | formatDate}}&ensp;&#xe74b;				
+				{{startTime | formatDate}}&ensp;&#xe74b;
 			</text>
 			<text style="padding: 0 20rpx;color: #666666;">-</text>
 			<text class="time-range" @tap="onStartTimeTap(1,endTime)">
@@ -13,21 +13,23 @@
 			</text>
 		</view>
 		<view class="v-btn">
-			<text class="t-btn btn-speedy">月份选择</text>
+			<text class="t-btn btn-speedy" @tap="showMonthPicker = true">月份选择</text>
 			<text class="t-btn btn-report">生成报表</text>
 		</view>
 
 		<timePicker :requestCode="timeCode" :year="dialogTime.year" :month="dialogTime.month" :day="dialogTime.day" :show="showTimePicker"
 		 @onConfirm="timePickConfirm" @onCancel="showTimePicker = false"></timePicker>
-
+		<monthPicker :month="dialogTime.month" :show="showMonthPicker" @onConfirm="monthPickConfirm" @onCancel="showMonthPicker = false"></monthPicker>
 	</view>
 </template>
 
 <script>
 	import timePicker from '@/components/timePicker/timePicker.vue';
+	import monthPicker from '@/components/monthPicker/monthPicker.vue';
 	export default {
 		components: {
-			timePicker
+			timePicker,
+			monthPicker
 		},
 
 		data() {
@@ -36,6 +38,7 @@
 				startTime: "",
 				endTime: "",
 				showTimePicker: false,
+				showMonthPicker: false,
 				dialogTime: {
 					year: 0,
 					month: 0,
@@ -78,6 +81,19 @@
 				} else {
 					this.endTime = time;
 				}
+			},
+			monthPickConfirm(time) {
+				const date = new Date(new Date().getFullYear(), time.month, 0)
+				this.startTime = {
+					year: date.getFullYear(),
+					month: time.month,
+					day: 1
+				};
+				this.endTime = {
+					year: date.getFullYear(),
+					month: time.month,
+					day: date.getDay()
+				};
 			}
 		}
 	}

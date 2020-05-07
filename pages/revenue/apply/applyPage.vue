74e7da1ddda9d43b9b89b7f7a75d06664dd859fb
cxs <2462523702@qq.com>
2020-05-07 22:58:16 +0800

    111

--- pages/revenue/apply/applyPage.vue
+++ pages/revenue/apply/applyPage.vue
@@ -4,9 +4,10 @@
 		<app-tabs v-model="tabIndex" :tabs="tabs" :fixed="true" :top="120"></app-tabs>
 		<view class="list-wrap">
 			<block v-for="(item,index) in tabs" :key="index">
-				<applyList :tab="item" :ref="'list'+index" v-if="index == tabIndex"/>
+				<applyList :tab="item" :ref="'list'+index" v-if="index == tabIndex" />
 			</block>
 		</view>
+		<view class="btn-add" @tap="revenueFrom">&#xe604;</view>
 	</view>
 </template>
 
@@ -33,13 +34,17 @@
 				tabs: bossTabs,
 			};
 		},
-		onReachBottom(){
+		onReachBottom() {
 			console.log(this.tabIndex)
-			let gv='list'+this.tabIndex;
+			let gv = 'list' + this.tabIndex;
 			this.$refs[gv][0].loadMore()
 		},
 		methods: {
-
+			revenueFrom() {
+				uni.navigateTo({
+					url:'../../revenueForm/revenueForm'
+				})
+			}
 		}
 
 	};

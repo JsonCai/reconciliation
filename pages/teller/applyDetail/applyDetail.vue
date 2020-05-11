2ab9e04082547d9b97c34924abe380e20e8262bf
duoduodym <306623777@.com>
2020-05-11 22:47:32 +0800

    111

--- pages/teller/applyDetail/applyDetail.vue
+++ pages/teller/applyDetail/applyDetail.vue
@@ -38,32 +38,42 @@
 				<text class="title-text">审批信息</text>
 			</view>
 			<view class="item-cross-line">
-				<view class="item-nowrap">
+				<view class="item-nowrap" v-if="detailForm.approvals">
 					<text>审批意见</text>
-					<text>通过</text>
+					<text >{{detailForm.approvals.approvalOpinion}}</text>
 				</view>
-				<view class="item-nowrap">
+				<view class="item-nowrap" v-if="detailForm.approvals">
 					<text>审批时间</text>
-					<text>2020-06-01</text>
+					<text>{{detailForm.approvals.createTime}}</text>
 				</view>
-				<view class="item-nowrap">
+				<view class="item-nowrap" v-if="detailForm.approvals && detailForm.approvals.approvalPerson">
 					<text>审批人</text>
-					<text>黄总</text>
+					<text>{{detailForm.approvals.approvalPerson.employeeName}}</text>
 				</view>
 			</view>
 			<view class="title item-wrap" v-if="detailForm.approvals">
 				<image src="../../../static/images/remit.png" class="icon-wallet mr-20"></image>
 				<text class="title-text">操作</text>
 			</view>
-			<view class="img-wrap fc-6">
+			<view class="img-wrap fc-6 mt-20">
 				<text class="fc-30 fc-6">凭据：</text>
-				<imgList :list="detailForm.paymentVoucherUrls" :isDisabled="true" />
+				<imgList :list="detailForm.paymentVoucherUrls" />
 			</view>
 			<view class="item-wrap">
 				<text>打款日期:</text>
-				<text>{{ detailForm.paymentTime}}</text>
+				<view class="inner-wrap" @tap="onStartTimeTap(1)">
+					<text v-if="!detailForm.paymentTime" class="fc-9">请选择打款日期</text>
+					<text v-else>{{ detailForm.paymentTime}}</text>
+					<text class="font-icon">&#xe662;</text>
+				</view>
+			</view>
+			<view class="big-btn-wrap">
+				<view class='btn confirm-btn' @tap="onPassTap">保存</view>
 			</view>
 		</view>
+		<timePicker :requestCode="timeCode"
+		:show="showTimePicker" @onConfirm="timePickConfirm" 
+		@onCancel="showTimePicker = false"></timePicker>
 	</view>
 </template>
 
@@ -85,17 +95,33 @@
 				detailForm: {},
 				showTimePicker: false,
 				timeCode: 0,
+				
 			}
 		},
 		methods: {
 			onPassTap() {
+				if(!this.detailForm.paymentTime){
+					uni.showToast({
+						 title: '请选择打款时间',
+						 icon:'none'
+					})
+					return
+				}
 				paymentExpence({
 						expenseAccountId: this.detailForm.expenseAccountId,
 						paymentTime: this.detailForm.paymentTime,
-						paymentVoucherUrls: this.detailForm.paymentVoucherUrls
+						paymentVoucherUrls: this.detailForm.paymentVoucherUrls = ['aaa']
 					})
 					.then(res => {
-						console.log(res)
+						if(res.code == '0'){
+							uni.showToast({
+								icon: 'none',
+								title: "打款成功"
+							})
+							setTimeout(()=>{
+								uni.navigateBack()
+							},500)
+						}
 					})
 					.catch(err => {
 						console.log(err)
@@ -121,7 +147,9 @@
 			applyDetail(option.id)
 				.then(res => {
 					console.log(res)
-					this.$set(this, "detailForm", res.data.data.expenseAccount)
+					if(res.code == '0'){
+						this.$set(this, "detailForm", res.data.expenseAccount)
+					}
 				})
 				.catch(err => {
 					console.log(err)

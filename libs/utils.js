65d44d201ad8c1781143b1144f07e4e92b627c34
duoduodym <306623777@.com>
2020-05-13 23:26:13 +0800

    1

--- libs/utils.js
+++ libs/utils.js
@@ -5,72 +5,73 @@
  */
 
 function getType(obj) {
-  //tostring 会返回对应不同的标签的构造函数
-  const toString = Object.prototype.toString;
-  const map = {
-      '[object Boolean]': 'boolean',
-      '[object Number]': 'number',
-      '[object String]': 'string',
-      '[object Function]': 'function',
-      '[object Array]': 'array',
-      '[object Date]': 'date',
-      '[object RegExp]': 'regExp',
-      '[object Undefined]': 'undefined',
-      '[object Null]': 'null',
-      '[object Object]': 'object'
-  };
-  return map[toString.call(obj)];
+	//tostring 会返回对应不同的标签的构造函数
+	const toString = Object.prototype.toString;
+	const map = {
+		'[object Boolean]': 'boolean',
+		'[object Number]': 'number',
+		'[object String]': 'string',
+		'[object Function]': 'function',
+		'[object Array]': 'array',
+		'[object Date]': 'date',
+		'[object RegExp]': 'regExp',
+		'[object Undefined]': 'undefined',
+		'[object Null]': 'null',
+		'[object Object]': 'object'
+	};
+	return map[toString.call(obj)];
 }
-export  function deepClone(data) {
-  var type = getType(data);
-  var obj = {};
-  if (type === 'array') {
-      obj = [];
-  } else if (type === 'object') {
-      obj = {};
-  } else {
-      //不再具有下一层次
-      return data;
-  }
-  if (type === 'array') {
-      for (var i = 0, len = data.length; i < len; i++) {
-          obj.push(deepClone(data[i]));
-      }
-  } else if (type === 'object') {
-      for (var key in data) {
-          obj[key] = deepClone(data[key]);
-      }
-  }
-  return obj;
+export function deepClone(data) {
+	var type = getType(data);
+	var obj = {};
+	if (type === 'array') {
+		obj = [];
+	} else if (type === 'object') {
+		obj = {};
+	} else {
+		//不再具有下一层次
+		return data;
+	}
+	if (type === 'array') {
+		for (var i = 0, len = data.length; i < len; i++) {
+			obj.push(deepClone(data[i]));
+		}
+	} else if (type === 'object') {
+		for (var key in data) {
+			obj[key] = deepClone(data[key]);
+		}
+	}
+	return obj;
 }
-export function resetDateFormat(date){
-	if(date.split(' ').length>1){
+export function resetDateFormat(date) {
+	if (date.split(' ').length > 1) {
 		return date
 	}
-	if(date.split('-').length>2){
-		return date+" 00:00:00"
+	if (date.split('-').length > 2) {
+		return date + " 00:00:00"
 	}
 }
 
-export function dateFtt(fmt,date) 
-{ //author: meizz 
- var o = { 
- "M+" : date.getMonth()+1,     //月份 
- "d+" : date.getDate(),     //日 
- "h+" : date.getHours(),     //小时 
- "m+" : date.getMinutes(),     //分 
- "s+" : date.getSeconds(),     //秒 
- "q+" : Math.floor((date.getMonth()+3)/3), //季度 
- "S" : date.getMilliseconds()    //毫秒 
- }; 
- if(/(y+)/.test(fmt)) 
- fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length)); 
- for(var k in o) 
- if(new RegExp("("+ k +")").test(fmt)) 
- fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length))); 
- return fmt; 
+export function dateFtt(fmt, date) { //author: meizz 
+	var o = {
+		"M+": date.getMonth() + 1, //月份 
+		"d+": date.getDate(), //日 
+		"h+": date.getHours(), //小时 
+		"m+": date.getMinutes(), //分 
+		"s+": date.getSeconds(), //秒 
+		"q+": Math.floor((date.getMonth() + 3) / 3), //季度 
+		"S": date.getMilliseconds() //毫秒 
+	};
+	if (/(y+)/.test(fmt))
+		fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+	for (var k in o)
+		if (new RegExp("(" + k + ")").test(fmt))
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
 }
 
-export function fmtMoney(val){
+export function fmtMoney(val) {
 	return val.toFixed(2)
 }
+
+
--- pages/me/me.vue
+++ pages/me/me.vue
@@ -1,15 +1,40 @@
 <template>
 	<view>
-		
+		<view class='btn confirm-btn' @tap="onLogin">微信授权登录</view>
 	</view>
 </template>
 
 <script>
+	import {login} from '@/api/login/login'
 	export default {
 		data() {
 			return {
-				
+
 			};
+		},
+		methods: {
+			onLogin() {
+				uni.login({
+				  provider: 'weixin',
+				  success: function (loginRes) {
+				    // 获取用户信息
+				    uni.getUserInfo({
+				      provider: 'weixin',
+				      success: function (res) {
+					   if(res.errMsg == 'getUserInfo:ok'){
+						   const openId = res.userInfo.openId
+						   login({
+							   wechatNumber:openId
+						   }).then(r => {
+							   const appToken = r['Set-Authorization']
+							   console.log(appToken)
+						   })
+					   }
+				      }
+				    });
+				  }
+				});
+			}
 		}
 	}
 </script>

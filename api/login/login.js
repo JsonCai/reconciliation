3e52ba231da40df152ffbf28098b553b93fa2ebb
wanghefei <306623777@qq.com>
2020-05-14 21:07:16 +0800

    111

--- api/login/login.js
+++ api/login/login.js
@@ -3,19 +3,19 @@ import {
 	baseUrl
 } from '../../config/config'
 
-let tenantId = '23945197-a5df-44bf-842f-106b42f80355'
+const tenantId = '23945197-a5df-44bf-842f-106b42f80355'
 
 export function login(data) {
-	console.log(data)
 	data.tenantId = tenantId
+	console.log(data)
 	return new Promise((resolve, reject) => {
 		uni.request({
 			url: baseUrl + '/employees/wechat-login',
 			method: 'POST',
 			data,
 			success: (res) => {
-				console.log('成功了')
-				resolve(res.header)
+				console.log(res.data,res.header)
+				resolve(res)
 			},
 			fail: (err) => {
 				console.log('失败了')

4fe9c647ea40e801ec81939677c12ffe2d08170e
cxs <2462523702@qq.com>
2020-05-19 22:13:21 +0800

    123123

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,77 +2,82 @@
 	<view class="container">
 		<view class="item-list" v-if="isLogin">
 			{{JSON.stringify(permissions)}}
-			<indexNavItem  @handleClick="applyClick('BossPage')" text="报销" roleName="老板" v-if="showApplyBoss"/>
-			<indexNavItem @handleClick="financeClick('BossPage')" text="财务" roleName="老板" v-if="showFinanceBoss"/>
-			<indexNavItem @handleClick="applyClick('buyer')" text="报销" roleName="采购" v-if="showApplyBuyer"/>
-			<indexNavItem @handleClick="revenueClick('revenue')" text="营收" roleName="营收" v-if="showRevenueRevenuer"/>
-			<indexNavItem @handleClick="applyClick('teller')" text="报销" roleName="出纳" v-if="showApplyCashier"/>
-			<indexNavItem @handleClick="revenueClick('teller')" text="营收" roleName="出纳" v-if="showApplyCashier"/>
-			<indexNavItem @handleClick="financeClick('revenue')" text="财务" roleName="出纳" v-if="showFinanceCashier"/>
-			<indexNavItem @handleClick="specialPayments" text="特殊收支" roleName="出纳" v-if="showFinanceCashier"/>
-			<indexNavItem @handleClick="applyClick('accountant')" text="报销" roleName="会计" v-if="showApplyAccountant"/>
-			<indexNavItem @handleClick="revenueClick('accountant')" text="营收" roleName="会计" v-if="showRevenueAccountant"/>
-			<indexNavItem @handleClick="handleClick" text="报表" roleName="会计" v-if="showStatementAccountant"/>
+			<indexNavItem @handleClick="applyClick('BossPage')" text="报销" roleName="老板" v-if="showApplyBoss" />
+			<indexNavItem @handleClick="financeClick('BossPage')" text="财务" roleName="老板" v-if="showFinanceBoss" />
+			<indexNavItem @handleClick="applyClick('buyer')" text="报销" roleName="采购" v-if="showApplyBuyer" />
+			<indexNavItem @handleClick="revenueClick('revenue')" text="营收" roleName="营收" v-if="showRevenueRevenuer" />
+			<indexNavItem @handleClick="applyClick('teller')" text="报销" roleName="出纳" v-if="showApplyCashier" />
+			<indexNavItem @handleClick="revenueClick('teller')" text="营收" roleName="出纳" v-if="showApplyCashier" />
+			<indexNavItem @handleClick="financeClick('BossPage')" text="财务" roleName="出纳" v-if="showFinanceCashier" />
+			<indexNavItem @handleClick="specialPayments" text="特殊收支" roleName="出纳" v-if="showFinanceCashier" />
+			<indexNavItem @handleClick="applyClick('accountant')" text="报销" roleName="会计" v-if="showApplyAccountant" />
+			<indexNavItem @handleClick="revenueClick('accountant')" text="营收" roleName="会计" v-if="showRevenueAccountant" />
+			<indexNavItem @handleClick="statement" text="报表" roleName="会计" v-if="showStatementAccountant" />
 		</view>
-		<noLogin v-else/>
+		<noLogin v-else />
 	</view>
 </template>
 <script>
 	import indexNavItem from '@/components/indexNavItem/indexNavItem.vue'
 	import noLogin from '@/components/noLogin/noLogin.vue'
 	export default {
-		components:{
+		components: {
 			indexNavItem,
 			noLogin
 		},
 		data() {
 			return {
-				showApplyBoss:true,
-				showFinanceBoss:true,
-				showApplyBuyer:true,
-				showApplyRevenue:true,
-				showCashierApply:true,
-				showApplyCashier:true,
-				showFinanceApply:true,
-				showRevenueRevenuer:true,
-				showStatementAccountant:true,
-				showRevenueAccountant:true,
-				showApplyAccountant:true,
-				showFinanceCashier:true
+				showApplyBoss: true,
+				showFinanceBoss: true,
+				showApplyBuyer: true,
+				showApplyRevenue: true,
+				showCashierApply: true,
+				showApplyCashier: true,
+				showFinanceApply: true,
+				showRevenueRevenuer: true,
+				showStatementAccountant: true,
+				showRevenueAccountant: true,
+				showApplyAccountant: true,
+				showFinanceCashier: true
 			}
 		},
-		computed:{
-			permissions(){
+		computed: {
+			permissions() {
 				return this.$store.state.permissions
 			},
-			isLogin(){
+			isLogin() {
 				console.log(this.$store.state.token)
-				  if(this.$store.state.token) return true
-				  else return false
+				if (this.$store.state.token) return true
+				else return false
 			}
 		},
 		methods: {
-			applyClick(type){
+			applyClick(type) {
 				uni.navigateTo({
-					url:'/pages/'+type+'/apply/applyPage'
+					url: '/pages/' + type + '/apply/applyPage'
 				})
 			},
-			financeClick(type){
+			financeClick(type) {
 				uni.navigateTo({
-					url:'/pages/'+type+'/finance/finance'
+					url: '/pages/' + type + '/finance/finance'
 				})
 			},
-			revenueClick(type){
+			revenueClick(type) {
 				uni.navigateTo({
-					url:'../'+type+'/revenue/revenuePage'
+					url: '../' + type + '/revenue/revenuePage'
 				})
 			},
-			specialPayments(){
+			specialPayments() {
 				uni.navigateTo({
-					url:'../specialPayments/paymentList/paymentList'
+					url: '../specialPayments/paymentList/paymentList'
 				})
 			},
-			handleClick(type){
+			statement() {
+				uni.navigateTo({
+					url:'../reportScope/reportScope'
+				})
+			},
+			handleClick(type) {
 				// if(type == 'revence'){
 				// 	uni.navigateTo({
 				// 		url:
